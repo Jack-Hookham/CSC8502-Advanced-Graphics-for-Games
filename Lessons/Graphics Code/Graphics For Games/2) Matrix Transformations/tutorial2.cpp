b093978e7aceb508b25d7e76f8762fcdfd747305
Jack Hookham (PGT) <jackhookham@gmail.com>
2017-11-02 12:49:11 +0000

    Tutorial 2A - Vertex Transformation

--- Lessons/Graphics Code/Graphics For Games/2) Matrix Transformations/tutorial2.cpp
+++ Lessons/Graphics Code/Graphics For Games/2) Matrix Transformations/tutorial2.cpp
@@ -3,22 +3,26 @@
 #include "../../nclGL/window.h"
 #include "Renderer.h"
 
-int main() {
+int main() 
+{
 	Window w("Vertex Transformation!",800,600,false);
-	if(!w.HasInitialised()) {
+	if(!w.HasInitialised()) 
+	{
 		return -1;
 	}
 
 	Renderer renderer(w);
-	if(!renderer.HasInitialised()) {
+	if(!renderer.HasInitialised()) 
+	{
 		return -1;
 	}
 
 	float scale		= 100.0f;
 	float rotation	= 0.0f;
-	Vector3 position(0, 0, -1500.0f);
+	Vector3 position(0, 0, -3000.0f);
 
-	while(w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE)){
+	while(w.UpdateWindow() && !Window::GetKeyboard()->KeyDown(KEYBOARD_ESCAPE))
+	{
 		if(Window::GetKeyboard()->KeyDown(KEYBOARD_1)) 
 			renderer.SwitchToOrthographic();
 		if(Window::GetKeyboard()->KeyDown(KEYBOARD_2))
