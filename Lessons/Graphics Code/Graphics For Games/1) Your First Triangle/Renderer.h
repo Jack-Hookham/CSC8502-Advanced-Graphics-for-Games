8f4c659b65a18bea0c10003c2c00af03931f461b
Jack Hookham <jackhookham@gmail.com>
2017-11-05 20:41:46 +0000

    Tutorial 6 started

--- Lessons/Graphics Code/Graphics For Games/1) Your First Triangle/Renderer.h
+++ Lessons/Graphics Code/Graphics For Games/1) Your First Triangle/Renderer.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../../nclgl/OGLRenderer.h"
+#include "../nclgl/OGLRenderer.h"
 
 class Renderer : public OGLRenderer	{
 public:
---
+++ Lessons/Graphics Code/nclgl/SceneNode.cpp
@@ -0,0 +1,47 @@
+#include "SceneNode.h"
+
+SceneNode::SceneNode(Mesh* m, Vector4 colour)
+{
+	this->mesh = mesh;
+	this->colour = colour;
+	parent = NULL;
+	modelScale = Vector3(1, 1, 1);
+}
+
+SceneNode::~SceneNode(void)
+{
+	for (unsigned int i = 0; i < children.size(); ++i)
+	{
+		delete children[i];
+	}
+}
+
+void SceneNode::AddChild(SceneNode* s)
+{
+	children.push_back(s);
+	s->parent = this;
+}
+
+void SceneNode::Update(float msec)
+{
+	//This node has a parent...
+	if (parent)
+	{
+		worldTransform = parent->worldTransform * transform;
+	}
+	//Root node, world transform is local transform
+	else 
+	{
+		worldTransform = transform;
+	}
+
+	for (vector<SceneNode*>::iterator i = children.begin(); i != children.end(); ++i)
+	{
+		(*i)->Update(msec);
+	}
+}
+
+void SceneNode::Draw(const OGLRenderer& r)
+{
+	if (mesh) { mesh->Draw(); }
+}
