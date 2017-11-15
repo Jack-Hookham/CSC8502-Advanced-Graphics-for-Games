b255b73db6096dde176fa4e1d01bba69f4535056
Jack Hookham (PGT) <jackhookham@gmail.com>
2017-11-15 17:06:14 +0000

    Shadow debug planes

--- Graphics Code/Graphics For Games/Graphics Coursework/SolarSystem.cpp
+++ Graphics Code/Graphics For Games/Graphics Coursework/SolarSystem.cpp
@@ -77,37 +77,37 @@ SolarSystem::SolarSystem()
 	sun->AddChild(ico);
 
 	SolarObject* planeSouth = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeSouth->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeSouth->SetModelScale(Vector3(1500.0f, 1500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeSouth->SetTransform(Matrix4::Translation(Vector3(0.0f, 0.0f, 2000.0f)));
 	AddChild(planeSouth);
 
 	SolarObject* planeNorth = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeNorth->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeNorth->SetModelScale(Vector3(1500.0f, 1500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeNorth->SetTransform(Matrix4::Translation(Vector3(0.0f, 0.0f, -2000.0f)));
 	AddChild(planeNorth);
 
 	SolarObject* planeEast = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeEast->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeEast->SetModelScale(Vector3(1500.0f, 1500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeEast->SetTransform(Matrix4::Translation(Vector3(2000.0f, 0.0f, 0.0f)) * Matrix4::Rotation(-90.0f, Vector3(0, 1, 0)));
 	AddChild(planeEast);
 
 	SolarObject* planeWest = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeWest->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeWest->SetModelScale(Vector3(1500.0f, 1500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeWest->SetTransform(Matrix4::Translation(Vector3(-2000.0f, 0.0f, 0.0f)) * Matrix4::Rotation(90.0f, Vector3(0, 1, 0)));
 	AddChild(planeWest);
 
 	SolarObject* planeUp = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeUp->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeUp->SetModelScale(Vector3(500.0f, 500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeUp->SetTransform(Matrix4::Translation(Vector3(0.0f, -2000.0f, 0.0f)) * Matrix4::Rotation(90.0f, Vector3(1, 0, 0)));
 	AddChild(planeUp);
 
 	SolarObject* planeDown = new SolarObject(Mesh::GenerateQuad(), Vector4(1.0f, 1.0f, 1.0f, 1.0f), SolarType::TYPE_PLANET);
-	planeDown->SetModelScale(Vector3(1000.0f, 1000.0f, 1000.0f));
+	planeDown->SetModelScale(Vector3(500.0f, 500.0f, 1.0f));
 	//sun->SetModelScale(Vector3(10.0f, 10.0f, 10.0f));
 	planeDown->SetTransform(Matrix4::Translation(Vector3(0.0f, 2000.0f, 0.0f)) * Matrix4::Rotation(90.0f, Vector3(1, 0, 0)));
 	AddChild(planeDown);
@@ -120,7 +120,7 @@ SolarSystem::~SolarSystem()
 void SolarSystem::Update(float msec)
 {
 	//transform = transform * Matrix4::Rotation(msec / 10.0f, Vector3(0, 1, 0));
-	float sunRotate = 1.0f;
+	float sunRotate = 0.5f;
 	float earthOrbitSpeed = 0.2f;
 	float planet2OrbitSpeed = 0.2f;
 	float planet3OrbitSpeed = 0.2f;
