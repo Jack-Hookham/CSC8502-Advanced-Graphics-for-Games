63d680ff77aeb7712ff4d8bbf08a8c5f79a6c83a
Jack Hookham (PGT) <jackhookham@gmail.com>
2017-11-21 19:30:48 +0000

    home

--- Graphics Code/Shaders/CW/sobelFragment.glsl
+++ Graphics Code/Shaders/CW/sobelFragment.glsl
@@ -2,18 +2,7 @@
 
 uniform sampler2D diffuseTex;
 uniform vec2 pixelSize;
-
-vec2 textureCoordinate;
-vec2 leftTextureCoordinate;
-vec2 rightTextureCoordinate;
-
-vec2 topTextureCoordinate;
-vec2 topLeftTextureCoordinate;
-vec2 topRightTextureCoordinate;
-
-vec2 bottomTextureCoordinate;
-vec2 bottomLeftTextureCoordinate;
-vec2 bottomRightTextureCoordinate;
+uniform int isVertical;
 
 in Vertex {
 	vec4 	colour;
@@ -25,47 +14,38 @@ in Vertex {
 
 out vec4 fragColour;
 
-vec3 W = vec3(0.2125, 0.7154, 0.0721);
+mat3 sx = mat3( 
+    1.0, 2.0, 1.0, 
+    0.0, 0.0, 0.0, 
+   -1.0, -2.0, -1.0 
+);
+mat3 sy = mat3( 
+    1.0, 0.0, -1.0, 
+    2.0, 0.0, -2.0, 
+    1.0, 0.0, -1.0 
+);
 
 void main(void)
 {
-	float luminance = dot(texture2D(diffuseTex, IN.texCoord).rgb, W);
+    vec3 diffuse = texture(diffuseTex, IN.texCoord.st).rgb;
+	 fragColour = vec4(diffuse, 1.0);
+	 return;
 
-	fragColour = vec4(vec3(luminance), 1.0);
+    mat3 I;
+    for (int i = 0; i < 3; i++) 
+	{
+        for (int j = 0; j < 3; j++) 
+		{
+            vec3 sample = texelFetch(diffuseTex, ivec2(gl_FragCoord) + ivec2(i - 1, j - 1), 0 ).rgb;
+            I[i][j] = length(sample); 
+    	}
+	}
 
-	float imageWidthFactor = pixelSize.x;
-	float imageHeightFactor = pixelSize.y;
+	float gx = dot(sx[0], I[0]) + dot(sx[1], I[1]) + dot(sx[2], I[2]); 
+	float gy = dot(sy[0], I[0]) + dot(sy[1], I[1]) + dot(sy[2], I[2]);
 
-	vec2 widthStep = vec2(imageWidthFactor, 0.0);
-    vec2 heightStep = vec2(0.0, imageHeightFactor);
-	vec2 widthHeightStep = vec2(imageWidthFactor, imageHeightFactor);
-	vec2 widthNegativeHeightStep = vec2(imageWidthFactor, -imageHeightFactor);
-
-	textureCoordinate = IN.texCoord.xy;
-    leftTextureCoordinate = IN.texCoord.xy - widthStep;
-    rightTextureCoordinate = IN.texCoord.xy + widthStep;
-
-    topTextureCoordinate = IN.texCoord.xy + heightStep;
-    topLeftTextureCoordinate = IN.texCoord.xy - widthNegativeHeightStep;
-    topRightTextureCoordinate = IN.texCoord.xy + widthHeightStep;
-
-    bottomTextureCoordinate = IN.texCoord.xy - heightStep;
-    bottomLeftTextureCoordinate = IN.texCoord.xy - widthHeightStep;
-    bottomRightTextureCoordinate = IN.texCoord.xy + widthNegativeHeightStep;
-
-	float i00   = texture2D(diffuseTex, textureCoordinate).r;
-    float im1m1 = texture2D(diffuseTex, bottomLeftTextureCoordinate).r;
-    float ip1p1 = texture2D(diffuseTex, topRightTextureCoordinate).r;
-    float im1p1 = texture2D(diffuseTex, topLeftTextureCoordinate).r;
-    float ip1m1 = texture2D(diffuseTex, bottomRightTextureCoordinate).r;
-    float im10 = texture2D(diffuseTex, leftTextureCoordinate).r;
-    float ip10 = texture2D(diffuseTex, rightTextureCoordinate).r;
-    float i0m1 = texture2D(diffuseTex, bottomTextureCoordinate).r;
-    float i0p1 = texture2D(diffuseTex, topTextureCoordinate).r;
-    float h = -im1p1 - 2.0 * i0p1 - ip1p1 + im1m1 + 2.0 * i0m1 + ip1m1;
-    float v = -im1m1 - 2.0 * im10 - im1p1 + ip1m1 + 2.0 * ip10 + ip1p1;
-
-    float mag = length(vec2(h, v));
-
-	fragColour *= vec4(vec3(mag), 1.0);
-}
+	float g = sqrt(pow(gx, 2.0) + pow(gy, 2.0));
+	g = smoothstep(0.9, 0.9, g);
+	
+	fragColour = vec4(diffuse - vec3(g), 1.0);
+} 
